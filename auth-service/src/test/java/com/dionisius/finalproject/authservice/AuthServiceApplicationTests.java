636eaa4d0f51d5ce47469a8805f1a802325af2e5
Dionisius Damarta Yapenrui <chrisdionisius@gmail.com>
2021-12-29 20:09:56 +0700

    init auth-service

---
+++ auth-service/src/test/java/com/dionisius/finalproject/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dionisius.finalproject.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
